0062bcea57385e428b30904b71432697cae74350
Aryaaazrr <94944128+Aryaaazrr@users.noreply.github.com>
2025-04-28 16:42:51 +0700

    fix(route) : updated routing

--- routes/web.php
+++ routes/web.php
@@ -91,69 +91,101 @@ Route::middleware(['auth'])->group(function () {
     });
 
     Route::group(['middleware' => 'admin'], function () {
-        Route::get('admin/dashboard', [DashboardController::class, 'index'])->name('pegawai.dashboard');
-        Route::get('admin/dashboard/line-chart-anggota', [DashboardController::class, 'index'])->name('pegawai.line.chart');
-        Route::get('admin/dashboard/pie-chart-anggota', [DashboardController::class, 'index'])->name('pegawai.pie.chart');
+        Route::get('admin/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
+        Route::get('admin/dashboard/line-chart-anggota', [DashboardController::class, 'index'])->name('admin.line.chart');
+        Route::get('admin/dashboard/pie-chart-anggota', [DashboardController::class, 'index'])->name('admin.pie.chart');
 
-        Route::get('admin/anggota', [AnggotaController::class, 'index'])->name('pegawai.anggota');
-        Route::get('admin/anggota/add', [AnggotaController::class, 'create'])->name('pegawai.anggota.create');
-        Route::post('admin/anggota/add', [AnggotaController::class, 'store'])->name('pegawai.anggota.store');
-        Route::get('admin/anggota/edit/{id}', [AnggotaController::class, 'edit'])->name('pegawai.anggota.edit');
-        Route::put('admin/anggota/edit/{id}', [AnggotaController::class, 'update'])->name('pegawai.anggota.update');
-        Route::get('admin/anggota/export/pdf', [AnggotaController::class, 'export'])->name('pegawai.anggota.export');
+        Route::get('admin/anggota', [AnggotaController::class, 'index'])->name('admin.anggota');
+        Route::get('admin/anggota/add', [AnggotaController::class, 'create'])->name('admin.anggota.create');
+        Route::post('admin/anggota/add', [AnggotaController::class, 'store'])->name('admin.anggota.store');
+        Route::get('admin/anggota/edit/{id}', [AnggotaController::class, 'edit'])->name('admin.anggota.edit');
+        Route::put('admin/anggota/edit/{id}', [AnggotaController::class, 'update'])->name('admin.anggota.update');
+        Route::get('admin/anggota/export/pdf', [AnggotaController::class, 'export'])->name('admin.anggota.export');
 
-        Route::get('admin/simpanan', [SimpananController::class, 'index'])->name('pegawai.simpanan');
-        Route::get('admin/simpanan/add', [SimpananController::class, 'create'])->name('pegawai.simpanan.create');
-        Route::post('admin/simpanan/add', [SimpananController::class, 'store'])->name('pegawai.simpanan.store');
-        Route::get('admin/simpanan/view/{id}', [SimpananController::class, 'show'])->name('pegawai.simpanan.show');
-        Route::get('admin/simpanan/edit/{id}', [SimpananController::class, 'edit'])->name('pegawai.simpanan.edit');
-        Route::put('admin/simpanan/edit/{id}', [SimpananController::class, 'update'])->name('pegawai.simpanan.update');
-        Route::get('admin/simpanan/export/pdf/{id}', [SimpananController::class, 'export'])->name('pegawai.simpanan.export');
+        Route::get('admin/simpanan', [SimpananController::class, 'index'])->name('admin.simpanan');
+        Route::get('admin/simpanan/add', [SimpananController::class, 'create'])->name('admin.simpanan.create');
+        Route::post('admin/simpanan/add', [SimpananController::class, 'store'])->name('admin.simpanan.store');
+        Route::get('admin/simpanan/view/{id}', [SimpananController::class, 'show'])->name('admin.simpanan.show');
+        Route::get('admin/simpanan/edit/{id}', [SimpananController::class, 'edit'])->name('admin.simpanan.edit');
+        Route::put('admin/simpanan/edit/{id}', [SimpananController::class, 'update'])->name('admin.simpanan.update');
+        Route::get('admin/simpanan/export/pdf/{id}', [SimpananController::class, 'export'])->name('admin.simpanan.export');
 
-        Route::get('admin/pinjaman', [PinjamanController::class, 'index'])->name('pegawai.pinjaman');
-        Route::get('admin/pinjaman/add', [PinjamanController::class, 'create'])->name('pegawai.pinjaman.create');
-        Route::post('admin/pinjaman/add', [PinjamanController::class, 'store'])->name('pegawai.pinjaman.store');
-        Route::get('admin/pinjaman/view/{id}', [PinjamanController::class, 'show'])->name('pegawai.pinjaman.show');
-        Route::get('admin/pinjaman/edit/{id}', [PinjamanController::class, 'edit'])->name('pegawai.pinjaman.edit');
-        Route::get('admin/pinjaman/kredit', [PinjamanController::class, 'edit'])->name('pegawai.pinjaman.edit');
-        Route::get('admin/pinjaman/diragukan', [PinjamanController::class, 'dataDiragukan'])->name('pegawai.pinjaman.diragukan');
-        Route::get('admin/pinjaman/macet', [PinjamanController::class, 'dataMacet'])->name('pegawai.pinjaman.macet');
-        Route::put('admin/pinjaman/edit/{id}', [PinjamanController::class, 'update'])->name('pegawai.pinjaman.update');
-        Route::get('admin/pinjaman/export/pdf/{id}', [PinjamanController::class, 'export'])->name('pegawai.pinjaman.export');
+        Route::get('admin/pinjaman', [PinjamanController::class, 'index'])->name('admin.pinjaman');
+        Route::get('admin/pinjaman/add', [PinjamanController::class, 'create'])->name('admin.pinjaman.create');
+        Route::post('admin/pinjaman/add', [PinjamanController::class, 'store'])->name('admin.pinjaman.store');
+        Route::get('admin/pinjaman/belum-lunas', [PinjamanController::class, 'belumLunas'])->name('admin.pinjaman.belum.lunas');
+        Route::get('admin/pinjaman/lunas', [PinjamanController::class, 'lunas'])->name('admin.pinjaman.lunas');
+        Route::get('admin/pinjaman/view/{id}', [PinjamanController::class, 'show'])->name('admin.pinjaman.show');
+        Route::get('admin/pinjaman/edit/{id}', [PinjamanController::class, 'edit'])->name('admin.pinjaman.edit');
+        Route::get('admin/pinjaman/kredit', [PinjamanController::class, 'edit'])->name('admin.pinjaman.edit');
+        Route::get('admin/pinjaman/diragukan', [PinjamanController::class, 'dataDiragukan'])->name('admin.pinjaman.diragukan');
+        Route::get('admin/pinjaman/macet', [PinjamanController::class, 'dataMacet'])->name('admin.pinjaman.macet');
+        Route::put('admin/pinjaman/edit/{id}', [PinjamanController::class, 'update'])->name('admin.pinjaman.update');
+        Route::get('admin/pinjaman/export/pdf/{id}', [PinjamanController::class, 'export'])->name('admin.pinjaman.export');
 
-        Route::get('admin/laporan', [LaporanController::class, 'index'])->name('pegawai.laporan');
-        Route::post('admin/laporan/add', [LaporanController::class, 'store'])->name('pegawai.laporan.store');
-        Route::put('admin/laporan/update', [LaporanController::class, 'update'])->name('pegawai.laporan.update');
-        Route::get('admin/laporan/export/pdf', [LaporanController::class, 'export'])->name('pegawai.laporan.export');
+        Route::get('admin/laporan', [LaporanController::class, 'index'])->name('admin.laporan');
+        Route::post('admin/laporan/add', [LaporanController::class, 'store'])->name('admin.laporan.store');
+        Route::put('admin/laporan/update', [LaporanController::class, 'update'])->name('admin.laporan.update');
+        Route::get('admin/laporan/{id}', [LaporanController::class, 'destroy'])->name('admin.laporan.destroy');
+        Route::get('admin/laporan/export/pdf', [LaporanController::class, 'export'])->name('admin.laporan.export');
 
-        Route::get('admin/rekap', [RekapTransaksiController::class, 'index'])->name('pegawai.rekap');
-        Route::get('admin/rekap/filter', [RekapTransaksiController::class, 'index'])->name('pegawai.rekap.filter');
-        Route::get('admin/rekap/export/pdf', [RekapTransaksiController::class, 'export'])->name('pegawai.rekap.export');
+        Route::get('admin/rekap', [RekapTransaksiController::class, 'index'])->name('admin.rekap');
+        Route::get('admin/rekap/filter', [RekapTransaksiController::class, 'index'])->name('admin.rekap.filter');
+        Route::get('admin/rekap/export/pdf', [RekapTransaksiController::class, 'export'])->name('admin.rekap.export');
 
-        Route::get('admin/profile', [ProfileController::class, 'index'])->name('pegawai.profile');
-        Route::put('admin/profile/{id}', [ProfileController::class, 'update'])->name('pegawai.profile.update');
+        Route::get('admin/profile', [ProfileController::class, 'index'])->name('admin.profile');
+        Route::put('admin/profile/{id}', [ProfileController::class, 'update'])->name('admin.profile.update');
     });
 
     Route::group(['middleware' => 'anggota'], function () {
         Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
-        Route::get('dashboard/line-chart-anggota', [DashboardController::class, 'index'])->name('line.chart');
-        Route::get('dashboard/pie-chart-anggota', [DashboardController::class, 'index'])->name('pie.chart');
+        // Route::get('dashboard/line-chart-anggota', [DashboardController::class, 'index'])->name('line.chart');
+        // Route::get('dashboard/pie-chart-anggota', [DashboardController::class, 'index'])->name('pie.chart');
 
-        Route::get('pegawai', [PegawaiController::class, 'index'])->name('pegawai');
-        Route::get('pegawai/add', [PegawaiController::class, 'create'])->name('pegawai.create');
-        Route::post('pegawai/add', [PegawaiController::class, 'store'])->name('pegawai.store');
-        Route::get('pegawai/edit/{id}', [PegawaiController::class, 'edit'])->name('pegawai.edit');
-        Route::put('pegawai/edit/{id}', [PegawaiController::class, 'update'])->name('pegawai.update');
-        Route::get('pegawai/{id}', [PegawaiController::class, 'destroy'])->name('pegawai.destroy');
-        Route::get('pegawai/export/pdf', [PegawaiController::class, 'export'])->name('pegawai.export');
+        // Route::get('pegawai', [PegawaiController::class, 'index'])->name('pegawai');
+        // Route::get('pegawai/add', [PegawaiController::class, 'create'])->name('pegawai.create');
+        // Route::post('pegawai/add', [PegawaiController::class, 'store'])->name('pegawai.store');
+        // Route::get('pegawai/edit/{id}', [PegawaiController::class, 'edit'])->name('pegawai.edit');
+        // Route::put('pegawai/edit/{id}', [PegawaiController::class, 'update'])->name('pegawai.update');
+        // Route::get('pegawai/{id}', [PegawaiController::class, 'destroy'])->name('pegawai.destroy');
+        // Route::get('pegawai/export/pdf', [PegawaiController::class, 'export'])->name('pegawai.export');
 
-        Route::get('anggota', [AnggotaController::class, 'index'])->name('anggota');
-        Route::get('anggota/add', [AnggotaController::class, 'create'])->name('anggota.create');
-        Route::post('anggota/add', [AnggotaController::class, 'store'])->name('anggota.store');
-        Route::get('anggota/edit/{id}', [AnggotaController::class, 'edit'])->name('anggota.edit');
-        Route::put('anggota/edit/{id}', [AnggotaController::class, 'update'])->name('anggota.update');
-        Route::get('anggota/{id}', [AnggotaController::class, 'destroy'])->name('anggota.destroy');
-        Route::get('anggota/export/pdf', [AnggotaController::class, 'export'])->name('anggota.export');
+        // Route::get('anggota', [AnggotaController::class, 'index'])->name('anggota');
+        // Route::get('anggota/add', [AnggotaController::class, 'create'])->name('anggota.create');
+        // Route::post('anggota/add', [AnggotaController::class, 'store'])->name('anggota.store');
+        // Route::get('anggota/edit/{id}', [AnggotaController::class, 'edit'])->name('anggota.edit');
+        // Route::put('anggota/edit/{id}', [AnggotaController::class, 'update'])->name('anggota.update');
+        // Route::get('anggota/{id}', [AnggotaController::class, 'destroy'])->name('anggota.destroy');
+        // Route::get('anggota/export/pdf', [AnggotaController::class, 'export'])->name('anggota.export');
+
+        Route::get('simpanan', [SimpananController::class, 'index'])->name('simpanan');
+        Route::get('simpanan/add', [SimpananController::class, 'create'])->name('simpanan.create');
+        Route::post('simpanan/add', [SimpananController::class, 'store'])->name('simpanan.store');
+        Route::get('simpanan/view/{id}', [SimpananController::class, 'show'])->name('simpanan.show');
+        Route::get('simpanan/edit/{id}', [SimpananController::class, 'edit'])->name('simpanan.edit');
+        Route::put('simpanan/edit/{id}', [SimpananController::class, 'update'])->name('simpanan.update');
+        Route::get('simpanan/export/pdf/{id}', [SimpananController::class, 'export'])->name('simpanan.export');
+
+        Route::get('pinjaman', [PinjamanController::class, 'index'])->name('pinjaman');
+        Route::get('pinjaman/add', [PinjamanController::class, 'create'])->name('pinjaman.create');
+        Route::post('pinjaman/add', [PinjamanController::class, 'store'])->name('pinjaman.store');
+        Route::get('pinjaman/view/{id}', [PinjamanController::class, 'show'])->name('pinjaman.show');
+        Route::get('pinjaman/edit/{id}', [PinjamanController::class, 'edit'])->name('pinjaman.edit');
+        // Route::get('pinjaman/kredit', [PinjamanController::class, 'edit'])->name('pinjaman.edit');
+        // Route::get('pinjaman/diragukan', [PinjamanController::class, 'dataDiragukan'])->name('pinjaman.diragukan');
+        // Route::get('pinjaman/macet', [PinjamanController::class, 'dataMacet'])->name('pinjaman.macet');
+        Route::put('pinjaman/edit/{id}', [PinjamanController::class, 'update'])->name('pinjaman.update');
+        Route::get('pinjaman/export/pdf/{id}', [PinjamanController::class, 'export'])->name('pinjaman.export');
+
+        Route::get('laporan', [LaporanController::class, 'index'])->name('laporan');
+        Route::post('laporan/add', [LaporanController::class, 'store'])->name('laporan.store');
+        Route::put('laporan/update', [LaporanController::class, 'update'])->name('laporan.update');
+        Route::get('laporan/{id}', [LaporanController::class, 'destroy'])->name('laporan.destroy');
+        Route::get('laporan/export/pdf', [LaporanController::class, 'export'])->name('laporan.export');
+
+        Route::get('rekap', [RekapTransaksiController::class, 'index'])->name('rekap');
+        Route::get('rekap/filter', [RekapTransaksiController::class, 'index'])->name('rekap.filter');
+        Route::get('rekap/export/pdf', [RekapTransaksiController::class, 'export'])->name('rekap.export');
 
         Route::get('profile', [ProfileController::class, 'index'])->name('profile');
         Route::put('profile/{id}', [ProfileController::class, 'update'])->name('profile.update');
